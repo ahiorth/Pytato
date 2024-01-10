e3fd7c976905653629e56dda05b54e45b704d537
ahiorth <aksel.hiorth@uis.no>
2024-01-10 13:30:15 +0100

    u func

--- doc/src/modules/func/src-func/codes.py
+++ doc/src/modules/func/src-func/codes.py
@@ -1,4 +1,6 @@
 #%%
+
+
 name='FRØY'
 chars=["Å", "Ø", "Æ"]
 new_chars=["AA","O","AE"]
@@ -6,4 +8,17 @@ new_name=name
 for ch,nch in zip(chars,new_chars):
     new_name = new_name.replace(ch, nch)
 
+#%%
+def replace_chars(name):
+    ''' replace Norwegian characters and space in names'''
+    chars=[" ", "/","Å", "Ø", "Æ"]
+    new_chars=["_","_","AA","O","AE"]
+    new_name = name
+    for ch,nch in zip(chars,new_chars):
+        new_name = new_name.replace(ch, nch)
+    return new_name
+ 
+#%%
+
+assert replace_chars('Å')=='Aq', "Should be Aq"
 # %%
